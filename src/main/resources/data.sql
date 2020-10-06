c0d79ac9edb13ab9dfa83719604422b8d4095b76
Marek Musiał <mmusialmarek@gmail.com>
2020-10-06 16:33:37 +0200

    created file with default inserts to database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO charity.categories (id, name) VALUES (1, 'ubrania');
+INSERT INTO charity.categories (id, name) VALUES (2, 'zabawki');
+INSERT INTO charity.categories (id, name) VALUES (3, 'agd i rtv');
+INSERT INTO charity.categories (id, name) VALUES (4, 'meble');
+INSERT INTO charity.categories (id, name) VALUES (5, 'inne');
