b671ecba8b26fedcb67349f92afe02417c7d83f9
Marek Musiał <mmusialmarek@gmail.com>
2020-10-06 16:18:33 +0200

    created entity Category

---
+++ src/main/java/pl/coderslab/charity/model/Category.java
@@ -0,0 +1,21 @@
+package pl.coderslab.charity.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = "id")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+}
