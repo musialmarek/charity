a007788711ad46f70d8fb286eb320fea73ef659e
Marek Musiał <mmusialmarek@gmail.com>
2020-10-06 17:31:53 +0200

    added default values to start up inserts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,8 @@ INSERT INTO charity.categories (id, name) VALUES (2, 'zabawki');
 INSERT INTO charity.categories (id, name) VALUES (3, 'agd i rtv');
 INSERT INTO charity.categories (id, name) VALUES (4, 'meble');
 INSERT INTO charity.categories (id, name) VALUES (5, 'inne');
+
+INSERT INTO charity.institutions (id, description, name) VALUES (1, 'Troszczymy się o najuboższych.', 'Pomagamy');
+INSERT INTO charity.institutions (id, description, name) VALUES (2, 'Naszym celem jest pomoc rodzinom, które ucierpiały z powodu klęsk żywiołowych', 'Lepsze jutro');
+INSERT INTO charity.institutions (id, description, name) VALUES (3, 'Chcemy, aby każdy bezdomny czuł, że ma miejsce na ziemi, gdzie będzie czuł się bezpieczny, bez mrozu i głodu.', 'Twój dom');
+INSERT INTO charity.institutions (id, description, name) VALUES (4, 'Cel i misja: pomoc osobom, którzy z powodu długów muszą ogłosić upadłość majątkową', 'Nowy Start');
