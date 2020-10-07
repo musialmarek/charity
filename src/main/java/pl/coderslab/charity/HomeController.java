f18207a47451ff5d3b2e29fe31600ea182c3a134
Marek Musiał <mmusialmarek@gmail.com>
2020-10-07 22:00:00 +0200

    added adding list of institutions to model

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -1,12 +1,22 @@
 package pl.coderslab.charity;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.coderslab.charity.institution.InstitutionService;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class HomeController {
+    private final InstitutionService institutionService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        log.debug("\n ADDING LIST OF ALL FOUNDATIONS TO MODEL");
+        model.addAttribute("foundations", institutionService.getAll());
         return "index";
     }
 }
