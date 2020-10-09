092031e6ab8da097e27cea73f8d427ca6af39a43
Marek Musiał <mmusialmarek@gmail.com>
2020-10-09 17:55:21 +0200

    added method to get all categories from database

--- src/main/java/pl/coderslab/charity/category/CategoryService.java
+++ src/main/java/pl/coderslab/charity/category/CategoryService.java
@@ -3,10 +3,17 @@ package pl.coderslab.charity.category;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import pl.coderslab.charity.model.Category;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class CategoryService {
     private final CategoryRepository categoryRepository;
+
+    public List<Category> getAll() {
+        return categoryRepository.findAll();
+    }
 }
