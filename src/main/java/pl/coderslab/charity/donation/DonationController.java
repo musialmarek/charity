c180b4b51bc2a1526103a86a225daa103b612599
Marek Musiał <mmusialmarek@gmail.com>
2020-10-10 22:03:43 +0200

    added adding list of all organizations to model

--- src/main/java/pl/coderslab/charity/donation/DonationController.java
+++ src/main/java/pl/coderslab/charity/donation/DonationController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.category.CategoryService;
+import pl.coderslab.charity.institution.InstitutionService;
 import pl.coderslab.charity.model.Donation;
 
 @Controller
@@ -16,6 +17,7 @@ import pl.coderslab.charity.model.Donation;
 public class DonationController {
     private final DonationService donationService;
     private final CategoryService categoryService;
+    private final InstitutionService institutionService;
 
     @GetMapping
     public String donationForm(Model model) {
@@ -24,6 +26,8 @@ public class DonationController {
         model.addAttribute("donation", donation);
         log.debug("ADDING ALL CATEGORIES TO MODEL");
         model.addAttribute("categories", categoryService.getAll());
+        log.debug("ADDING ALL ORGANIZATIONS TO MODEL");
+        model.addAttribute("organizations",institutionService.getAll());
         log.debug("SHOWING ADD-DONATION FORM");
         return "donation/form";
     }
