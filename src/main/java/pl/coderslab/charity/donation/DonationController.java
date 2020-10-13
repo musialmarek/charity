0b550e5e96b96f366ddbbb22bcca6fb46c134c26
Marek Musiał <mmusialmarek@gmail.com>
2020-10-13 10:48:40 +0200

    added actions to save Donation and show confirmation page

--- src/main/java/pl/coderslab/charity/donation/DonationController.java
+++ src/main/java/pl/coderslab/charity/donation/DonationController.java
@@ -5,21 +5,23 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.category.CategoryService;
 import pl.coderslab.charity.institution.InstitutionService;
+import pl.coderslab.charity.model.Category;
 import pl.coderslab.charity.model.Donation;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/donation/form")
+@RequestMapping("/donation")
 public class DonationController {
     private final DonationService donationService;
     private final CategoryService categoryService;
     private final InstitutionService institutionService;
 
-    @GetMapping
+    @GetMapping("/form")
     public String donationForm(Model model) {
         Donation donation = new Donation();
         log.debug("ADDING EMPTY DONATION TO MODEL");
@@ -27,8 +29,25 @@ public class DonationController {
         log.debug("ADDING ALL CATEGORIES TO MODEL");
         model.addAttribute("categories", categoryService.getAll());
         log.debug("ADDING ALL ORGANIZATIONS TO MODEL");
-        model.addAttribute("organizations",institutionService.getAll());
+        model.addAttribute("organizations", institutionService.getAll());
         log.debug("SHOWING ADD-DONATION FORM");
         return "donation/form";
+    }
+
+    @PostMapping("/form")
+    public String saveDonation(Donation donation) {
+        for (Category category : donation.getCategories()) {
+            log.debug("\nSAVING DONATION HAS CATEGORY: {}",category.getName());
+        }
+        Donation saved = donationService.save(donation);
+        if(saved!=null&&saved.getId()!=null){
+            return "redirect:/donation/success";
+        }
+        return "redirect:/donation/form";
+    }
+
+    @GetMapping("/success")
+    public String confirmDonation(){
+        return "donation/form-confirmation";
     }
 }
